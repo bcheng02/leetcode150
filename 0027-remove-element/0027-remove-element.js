bda53d7bf568fc35309f09b6664267da7ee4a5f3
bananaplant7 <90235334+bananaplant7@users.noreply.github.com>
2023-09-21 22:48:35 -0700

    Time: 53 ms (52.67%), Space: 41.7 MB (80.68%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -3,8 +3,21 @@
  * @param {number} val
  * @return {number}
  */
+// var removeElement = function(nums, val) {
+//     let filtered = nums.filter(x => x !== val)
+//     nums.forEach((_, i) => nums[i] = filtered[i])
+//     return filtered.length
+// };
+
 var removeElement = function(nums, val) {
-    let filtered = nums.filter(x => x !== val)
-    nums.forEach((_, i) => nums[i] = filtered[i])
-    return filtered.length
+    let index = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+
+    return index;
 };
