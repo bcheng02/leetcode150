3bc1c8080da960201ec96938dcb2e4c08e8eba48
bananaplant7 <90235334+bananaplant7@users.noreply.github.com>
2023-09-25 17:24:14 -0700

    Time: 75 ms (80.76%), Space: 51.5 MB (69.09%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -5,33 +5,43 @@
  */
 var rotate = function(nums, k) {
 
-    let spliceIndex
-    let numLen = nums.length
+//     let spliceIndex
+//     let numLen = nums.length
 
  
-    spliceIndex = numLen - k
+//     spliceIndex = numLen - k
     
-    // splice works w/ negative numbers so long as arr[startIndex + arrSize] exists
-    while (spliceIndex + numLen < 0) {
-        spliceIndex += numLen
+//     // splice works w/ negative numbers so long as arr[startIndex + arrSize] exists
+//     while (spliceIndex + numLen < 0) {
+//         spliceIndex += numLen
+//     }
+
+
+//     let chunk = nums.splice(spliceIndex, k)
+//     let newArr = chunk.concat(nums)
+    
+//     newArr.forEach((_, i) => {
+//         nums[i] = newArr[i]
+//     })
+    
+    
+    
+    
+    
+    
+    
+    // change each index
+    let numLen = nums.length
+    let maxIndex = numLen - 1
+    let numsCopy = [...nums]
+    
+    for (let i = 0; i < numLen; i++) {
+        let swapIndex = i - k
+        while (swapIndex < 0) {
+            swapIndex += numLen
+        }
+        nums[i] = numsCopy[swapIndex]
     }
-
-
-    let chunk = nums.splice(spliceIndex, k)
-    let newArr = chunk.concat(nums)
-    
-    newArr.forEach((_, i) => {
-        nums[i] = newArr[i]
-    })
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
