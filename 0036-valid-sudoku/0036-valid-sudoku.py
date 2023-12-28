16417989e0e8608b35c8e068f5bfe7412c3d879f
bananaplant7 <90235334+bananaplant7@users.noreply.github.com>
2023-12-28 12:10:58 -0800

    Time: 126 ms (5.24%), Space: 17.5 MB (7.96%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,22 +1,112 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)
+        # check rows OK
+        # check cols OK
+        # check box ??
         
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".": 
-                    continue
-                if (board[r][c] in cols[c] or
-                    board[r][c] in rows[r] or
-                    board[r][c] in squares[(r // 3, c // 3)]):
-                    return False
-                   
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r//3, c//3)].add(board[r][c])
-        return True
+    
+    
+    
+        # collect cols into arrOfCols
+        # list comprehension needed?? YES!
+        arrOfCols = [["."] * 9 for i in range(9)]
+        for i in range(9):
+            for j in range(9):
+                arrOfCols[i][j] = board[j][i]
+                
+
+        # check no duplicates and 1-9 per row
+        for i in range(9):
+            arrOfNums = list(filter(lambda x: x.isnumeric() and int(x) >= 1 and int(x) <= 9 , board[i]))
+            if (len(set(arrOfNums)) != len(arrOfNums)):
+                return False
+
+        # check no duplicates and 1-9 per col
+        for i in range(9):
+            arrOfNums = list( filter(lambda x: x.isnumeric() and int(x) >= 1 and int(x) <= 9 , arrOfCols[i]) )
+            if (len(set(arrOfNums)) != len(arrOfNums)):
+                return False
+            
+        
+        # check 9 boxes?? this is STUPID
+        box1 = ["."] * 9
+        k = 0
+        for i in range(0, 3):
+            for j in range(0, 3):
+                box1[k] = board[i][j]
+                k+=1
+                
+
+        k = 0
+        box2 = ["."] * 9
+        for i in range(3, 6):
+            for j in range(0, 3):
+                box2[k] = board[j][i]
+                k+=1
 
                 
+        k = 0        
+        box3 = ["."] * 9
+        for i in range(6, 9):
+            for j in range(0, 3):
+                box3[k] = board[j][i]
+                k+=1
+ 
+                
+        k = 0        
+        box4 = ["."] * 9
+        for i in range(0, 3):
+            for j in range(3, 6):
+                box4[k] = board[j][i]
+                k+=1
+
+        
+        k = 0
+        box5 = ["."] * 9
+        for i in range(3, 6):
+            for j in range(3, 6):
+                box5[k] = board[j][i]
+                k+=1
+        
+        k = 0
+        box6 = ["."] * 9
+        for i in range(6, 9):
+            for j in range(3, 6):
+                box6[k] = board[j][i]
+                k+=1
+        
+        k = 0
+        box7 = ["."] * 9
+        for i in range(0, 3):
+            for j in range(6, 9):
+                box7[k] = board[j][i]
+                k+=1
+                
+        k = 0
+        box8 = ["."] * 9
+        for i in range(3, 6):
+            for j in range(6, 9):
+                box8[k] = board[j][i]
+                k+=1
+        
+        k = 0
+        box9 = ["."] * 9
+        for i in range(6, 9):
+            for j in range(6, 9):
+                box9[k] = board[j][i]
+                k+=1
+
+
+        arrOfBoxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
+        
+        print(arrOfBoxes)
+        
+        for i in range(9):
+            arrOfNums = list( filter(lambda x: x.isnumeric() and int(x) >= 1 and int(x) <= 9 , arrOfBoxes[i]) )
+            if (len(set(arrOfNums)) != len(arrOfNums)):
+                return False
             
+        return True
+        
+        
+    
