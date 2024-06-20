5793abacfef172ff02c570f7f9d44f3556d655c5
bcheng02 <90235334+bcheng02@users.noreply.github.com>
2024-06-20 13:04:17 -0700

    Time: 61 ms (8.18%) | Memory: 23.1 MB (5.75%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered = list(filter(lambda x: x.isalnum(), s))
+        filtered_lower = list(map(lambda x: x.lower(), filtered))
+        
+        if filtered_lower == []: return True
+        
+        # 2 pointers 
+        ptr1 = 0
+        ptr2 = len(filtered_lower) - 1
+        
+        while(ptr1 < ptr2):
+            if filtered_lower[ptr1] != filtered_lower[ptr2]:
+                return False
+            ptr1 += 1
+            ptr2 -= 1
+            
+        return True
+        
