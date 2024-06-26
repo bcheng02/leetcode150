0eca22a510287b8413ca8a059dc391032cb6d78a
bcheng02 <90235334+bcheng02@users.noreply.github.com>
2024-06-26 15:29:29 -0700

    Time: 35 ms (86.59%) | Memory: 12.1 MB (29.19%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -6,18 +6,46 @@ class Solution(object):
         :rtype: bool
         """
         
-        # look in window of sizes of len(s1) in s2, check to see if window contains all chars of s1
+        if len(s1) > len(s2):
+            return False
         
         start = 0
-        s1 = sorted(s1)
-    
-        for end in range(len(s1) - 1, len(s2)):
-            if (sorted(s2[start:end+1]) == s1):
-                return True
+        s1Arr = [0] * 26
+        s2Arr = [0] * 26
+        matches = 0
+        
+        for i in range(len(s1)):
+            s1Arr[ord(s1[i]) - ord('a')] += 1
+            s2Arr[ord(s2[i]) - ord('a')] += 1
+
+        for i in range(26):
+            if (s1Arr[i] == s2Arr[i]):
+                matches += 1
             
+        for end in range(len(s1), len(s2)): 
+            if matches == 26: return True
+            
+            # add new, update matches
+            index = ord(s2[end]) - ord('a')
+            s2Arr[index] += 1
+            if (s1Arr[index] == s2Arr[index]): 
+                matches += 1
+            elif (s1Arr[index] == s2Arr[index] - 1):
+                matches -= 1
+                
+            # remove old, update matches
+            index = ord(s2[start]) - ord('a')
+            s2Arr[index] -= 1
+            if (s1Arr[index] == s2Arr[index]): 
+                matches += 1
+            elif (s1Arr[index] == s2Arr[index] + 1):
+                matches -= 1
+                
             start += 1
-            
         
-        return False
+        return matches == 26
         
-     
+        
+        # make 2 arrays that represent letters of alphabet and their counts
+        # make a matches variable that compares how many letters match
+        # loop thru s2 in correct window sizes and update the pointers, arrays, and matches variable
