f068f2ab13462ad6b3f6d14dcad34001080c81cc
Brooklyn Cheng <90235334+bcheng02@users.noreply.github.com>
2024-12-26 14:06:24 -0800

    Time: 0 ms (100.00%) | Memory: 13.2 MB (9.70%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,14 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        map = {}
-        for i in range(len(nums)):
-            map[nums[i]] = i
+        map = {}            
         
         for i in range(len(nums)):
-            currVal = nums[i]
-            newTarg = target - currVal
-            
-            if (newTarg in map) and (map[newTarg] != i):
+            newTarg = target - nums[i]
+            if (newTarg in map): 
                 return [i, map[newTarg]]
+            else: map[nums[i]] = i
+            
+                
+# 2 pass
+#         map = {}
+#         for i in range(len(nums)):
+#             map[nums[i]] = i
+        
+#         for i in range(len(nums)):
+#             currVal = nums[i]
+#             newTarg = target - currVal
+            
+#             if (newTarg in map) and (map[newTarg] != i):
+#                 return [i, map[newTarg]]
         
