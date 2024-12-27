e502adb6b4b36f96f48efc0f12265e160c1436e6
Brooklyn Cheng <90235334+bcheng02@users.noreply.github.com>
2024-12-26 17:08:41 -0800

    Time: 675 ms (78.53%) | Memory: 18.3 MB (29.40%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,37 +1,25 @@
-import math
-
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = []
         nums.sort()
         
-        ans = []
-        
-        for i in range(len(nums)):
-            if (i > 0 and nums[i] == nums[i-1]): continue
+        for i, num in enumerate(nums):
+            if (i != 0 and num == nums[i - 1]): continue
             
-            ptr1 = i + 1
-            ptr2 = len(nums) - 1
-            while(ptr1 < ptr2):
-                sum = nums[ptr1] + nums[ptr2] + nums[i]
-                
-                if (sum < 0):
-                    ptr1 += 1
-                elif (sum > 0):
-                    ptr2 -= 1
+            l, r = i + 1, len(nums) - 1
+            while(l < r):
+                amount = num + nums[l] + nums[r]
+                if (amount < 0):
+                    l += 1
+                elif (amount > 0):
+                    r -= 1
                 else:
-                    ans.append([nums[i], nums[ptr1], nums[ptr2]])
-                    
-                    # if ptr1's next value is same, then want to keep going
-                    # only need to change ptr1 since ptr2 will be updated with other code
-                    ptr1 += 1
-                    while (nums[ptr1] == nums[ptr1 - 1] and ptr1 < ptr2):
-                        ptr1 += 1
-                    
-    
+                    ans.append([num, nums[l], nums[r]])
+                    l += 1
+                    while (nums[l] == nums[l - 1] and l < r):
+                        l += 1   
         return ans
-        
-        
-        
-        
-            
-        
