7d340329fa424df226d1c92fda913685a9af6401
Brooklyn Cheng <90235334+bcheng02@users.noreply.github.com>
2024-12-28 14:40:17 -0800

    Time: 163 ms (33.02%) | Memory: 12.9 MB (44.67%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,38 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        
-        start = 0
         maxLen = 0
+        start = 0
         count = {}
         
-        for end in range(len(s)):
-            count[s[end]] = 1 + count.get(s[end], 0)
-            
-            if (end-start+1 - max(count.values()) <= k):
-                maxLen = max(maxLen, end-start+1)
-                
-            else: 
+        for end, val in enumerate(s):
+            count[s[end]] = count.get(s[end], 0) + 1
+            if (end-start+1 - max(count.values())) <= k:
+                maxLen = max(end-start+1, maxLen)
+            else:
                 count[s[start]] -= 1
                 start += 1
-                maxLen = max(maxLen, end-start+1)
-                
-        
-        
+
         return maxLen
-            
-        
-        
-        # we know that the window will be at least size k and at most size n, since 
-            # 0 <= k <= s.length, so k will always be at most the size of the string
-            # then we can replace k adjacent chars with the same char
-        
-        # maxLen = max(maxLen, end-start+1)
-        
-        
-        # problems
-            # idk what exactly is going to make it longest repeating. it's not the most frequent and it's not the most consecutive..what's the trick is there even a trick???
-                # not most consecutive: c c b d b d b. most consec is c. correct repeating char is b
-                # not most frequent: 
-            # how can we have a "global picture" (know where things are and where to place the character without there being a trick) unless we have to iterate thru the list a bunch of times??? (up to n^k)
-            
