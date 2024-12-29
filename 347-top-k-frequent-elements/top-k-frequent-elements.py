c2766790221f6bca3ac8878942b496bd53447ebc
Brooklyn Cheng <90235334+bcheng02@users.noreply.github.com>
2024-12-28 18:25:00 -0800

    Time: 15 ms (11.33%) | Memory: 24.1 MB (5.12%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,9 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freqMap = {}
+        buckets = [[] for i in range(len(nums) + 1)]
 
         for i, val in enumerate(nums):
             freqMap[val] = freqMap.get(val, 0) + 1
+        for n, c in freqMap.items():
+            buckets[c].append(n)
+        
+        ans = []
+        for i in range(len(buckets) - 1, 0, -1):
+            for j in buckets[i]:
+                ans.append(j)
+                if len(ans) == k:
+                    return ans
 
-        freqMap = dict( sorted(freqMap.items(), key = lambda x: x[1], reverse=True) )
-        return list(freqMap.keys())[0:k] 
+        
+
