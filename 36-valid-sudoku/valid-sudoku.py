44ac115cd76c2e02d3778d1dda0a62a98413eb90
Brooklyn Cheng <90235334+bcheng02@users.noreply.github.com>
2024-12-30 11:37:01 -0800

    Time: 2 ms (86.98%) | Memory: 17.9 MB (6.09%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,29 +1,19 @@
-import math
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        validValues = ['1', '2', '3', '4', '5','6', '7', '8', '9']
-        subBoxes = {i:[] for i in range(len(board))}
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
 
-        for i, row in enumerate(board):
-            filteredRow = [val for val in row if val != "." and val in validValues]
-            if  len(filteredRow) != len(set(filteredRow)):
-                return False
-
-            col = []
-            for j in range(len(row)):
-                col.append(board[j][i])
-                boxNum = 3 * math.floor(i/3) + math.floor(j/3)
-                subBoxes[boxNum].append(board[j][i]) 
-            filteredCol = [val for val in col if val != "." and val in validValues]
-            if  len(filteredCol) != len(set(filteredCol)):
-                return False
-
-        print(subBoxes)
-        for box in subBoxes.values():
-            filteredBox = [val for val in box if val != "." and val in validValues]
-            if  len(filteredBox) != len(set(filteredBox)):
-                return False
-
-
-            
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                elif (board[r][c] in cols[c] or
+                     board[r][c] in rows[r] or
+                     board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                else:
+                    cols[c].add(board[r][c])
+                    rows[r].add(board[r][c])
+                    squares[(r//3, c//3)].add(board[r][c])
         return True
